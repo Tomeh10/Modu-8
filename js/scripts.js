98093244cde32565f150aeecebd6b3c4bb06635c
Tomeh10 <teha@vp.pl>
2017-04-03 21:11:58 +0200

    plik sjs

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//plik script.js
+/*
+W przeglądarce Chrome znajdź w menu Narzędzia dla programistów lub użyj skrótów:
+    Windows: ctrl + shift + i
+    Mac: alt + cmd + i
+    lub znajdź w menu Narzędzia dla programistów
+*/
