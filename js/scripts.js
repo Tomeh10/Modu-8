b09dcfc8df933080b81cda1eb7092244df968c03
Tomeh10 <teha@vp.pl>
2017-04-25 21:07:30 +0200

    plik sjs

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var a=4;
+var b=5;
+var value=(a * a) + (2 * a * b) - (b * b);
+if (value > 0) {
+//kod do wykonania jeśli warunek1 okaże się prawdziwy
+console.log("Wynik dodatni");
+}
+else if (value < 0) {
+//kod do wykonania, jeśli warunek2 okaże się prawdziwy
+}
+console.log("Wynik ujemny");
+else {
+//kod do wykonania, jeśli żaden z poprzednich warunków nie okaże się prawdziwy
+console.log("Wynik równy zero");
+}
