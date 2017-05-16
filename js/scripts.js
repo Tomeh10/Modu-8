784d19b54a62dae531fdca86c01a59e77018dab7
Tomeh10 <teha@vp.pl>
2017-05-17 01:22:21 +0200

    plik sjs

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 function getTriangleArea(a, h) {
-	// sprawdź, czy czy przekazane parametry a oraz h mają wartość większą od zera
-	// jeśli nie (będzie mniejszy bądź równy zero), to zwróć wartość 'Nieprawidłowe dane'
+ // sprawdź, czy czy przekazane parametry a oraz h mają wartość większą od zera
+ // jeśli nie (będzie mniejszy bądź równy zero), to zwróć wartość 'Nieprawidłowe dane'
+if (a > 0 && h > 0) {
+ //kod wykona się jeśli warunek jest spełniony
 var wynik = (a*h/2);
 return wynik;
+
+} else {
+ //kod wykona się w przeciwnym wypadku warunek nie zostanie spełniony
+var wynik = 'Błędnie wprowadzone dane ';
+return wynik;
 }
+
 var wartoscFunkcji = getTriangleArea(10,4);
 console.log( getTriangleArea(10,6) )
